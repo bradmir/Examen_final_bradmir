ebace3e0c12092e81236558bdb61fabcaf53f9e9
bradmir <bradmir007@gmail.com>
2016-06-18 10:22:47 -0400

    examen final

---
+++ routes/bjetoEjemplo.js
@@ -0,0 +1,15 @@
+var microprocesador ={
+marca :"INTEL",
+modelo :"Intel® Core™ i7-6700T Processor",
+cache:"8 MB",
+velocida_de_reloj:"2.80 GHZ",
+tipo_de_memoria:"DDR4",
+precio:400,
+
+}
+for (i in microprocesador){
+
+console.log(i+":"+microprocesador[i]);
+
+}
+
